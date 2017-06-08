0f3feb3b1c1801b63fffb170fa6dab027d18899d
maxe-101 <maxe-101@mail.ru>
2017-06-09 00:27:14 +0300

    razrab

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php include_once("razrab.html"); ?>
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["us-cdbr-iron-east-03.cleardb.net"];
+$username = $url["eroku_fda0a19831eaf8c"];
+$password = $url["e72cfa9e"];
+$db = substr($url["mysql://bbe32d767cdbfd:e72cfa9e@us-cdbr-iron-east-03.cleardb.net/heroku_fda0a19831eaf8c?reconnect=true"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+?>
