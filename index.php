aac8e50aebbc4db07bdc67a021e84921a2df073a
maxe-101 <maxe-101@mail.ru>
2017-06-10 01:09:12 +0300

    razrab

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["us-cdbr-iron-east-03.cleardb.net"];
-$username = $url["heroku_fda0a19831eaf8c"];
+$username = $url["bbe32d767cdbfd"];
 $password = $url["e72cfa9e"];
 $db = substr($url["mysql://bbe32d767cdbfd:e72cfa9e@us-cdbr-iron-east-03.cleardb.net/heroku_fda0a19831eaf8c?reconnect=true"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
 echo "1"; 
 ?>
+CLEARDB_DATABASE_URL => mysql://[username]:[password]@[host]/[database name]
