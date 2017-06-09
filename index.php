5c49e820b1b7f8345d7f13942426f78508019cbe
maxe-101 <maxe-101@mail.ru>
2017-06-10 01:24:28 +0300

    razrab

--- index.php
+++ index.php
@@ -8,5 +8,19 @@ $password = $url["e72cfa9e"];
 $db = substr($url["mysql://bbe32d767cdbfd:e72cfa9e@us-cdbr-iron-east-03.cleardb.net/heroku_fda0a19831eaf8c?reconnect=true"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
-echo "1"; 
+
+echo "1";
+$mysqli->query('SELECT * FROM new_table');
+$mysqli->query('SET foreign_key_checks = 0');
+if ($result = $mysqli->query("SHOW TABLES"))
+{
+    while($row = $result->fetch_array(MYSQLI_NUM))
+    {
+        $mysqli->query('DROP TABLE IF EXISTS '.$row[0]);
+    }
+}
+
+$mysqli->query('SET foreign_key_checks = 1');
+$mysqli->close();
+echo "2"; 
 ?>
