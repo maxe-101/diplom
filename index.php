3886347061d6f6cb86667302c5e93ae325efa845
maxe-101 <maxe-101@mail.ru>
2017-06-12 02:08:44 +0300

    razrab

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php include_once("razrab.html"); ?>
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["us-cdbr-iron-east-03.cleardb.net"];
