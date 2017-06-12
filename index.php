aa9844f5a18e376ac47c8c1748913f2cc2d16c41
maxe-101 <maxe-101@mail.ru>
2017-06-13 00:08:03 +0300

    razrab

--- index.php
+++ index.php
@@ -3,15 +3,17 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $server = $url["us-cdbr-iron-east-03.cleardb.net"];
 $username = $url["bbe32d767cdbfd"];
 $password = $url["e72cfa9e"];
 $db = substr($url["mysql://bbe32d767cdbfd:e72cfa9e@us-cdbr-iron-east-03.cleardb.net/heroku_fda0a19831eaf8c?reconnect=true"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
-
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 echo "1";
 $conn->query('SELECT * FROM new_table');
 if ($result = $conn->query("SHOW TABLES"))
@@ -23,5 +25,5 @@ if ($result = $conn->query("SHOW TABLES"))
 }
 
 $conn->close();
-echo "2"; 
+echo "2";
 ?>
