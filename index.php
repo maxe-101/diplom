9d74e77c8f3efe0b4a179cb81612fb0e810a025d
maxe-101 <maxe-101@mail.ru>
2017-06-13 00:25:24 +0300

    razrab

--- index.php
+++ index.php
@@ -4,18 +4,26 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$server = $url["us-cdbr-iron-east-03.cleardb.net"];
-$username = $url["bbe32d767cdbfd"];
-$password = $url["e72cfa9e"];
-$db = substr($url["mysql://bbe32d767cdbfd:e72cfa9e@us-cdbr-iron-east-03.cleardb.net/heroku_fda0a19831eaf8c?reconnect=true"], 1);
+$url = parse_url(getenv("mysql://bbe32d767cdbfd:e72cfa9e@us-cdbr-iron-east-03.cleardb.net/heroku_fda0a19831eaf8c"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+if ($conn->query("select * from new_table") === TRUE) {
+    echo "GOOOD MATHERFUCKA";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
 echo "1";
-$conn->query('SELECT * FROM new_table');
+/*$conn->query('SELECT * FROM new_table');
 if ($result = $conn->query("SHOW TABLES"))
 {
     while($row = $result->fetch_array(MYSQLI_NUM))
@@ -24,6 +32,6 @@ if ($result = $conn->query("SHOW TABLES"))
     }
 }
 
-$conn->close();
+$conn->close();*/
 echo "2";
 ?>
