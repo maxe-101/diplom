92730f5491b98a976f68cce2ac7da333b717daca
maxe-101 <maxe-101@mail.ru>
2017-06-13 03:46:20 +0300

    razrab

--- index.php
+++ index.php
@@ -4,34 +4,21 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
-$url = parse_url(getenv("mysql://bbe32d767cdbfd:e72cfa9e@us-cdbr-iron-east-03.cleardb.net/heroku_fda0a19831eaf8c?reconnect=true"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
+$server = "us-cdbr-iron-east-03.cleardb.net";
+$username = "bbe32d767cdbfd";
+$password = "e72cfa9e";
+$db = "heroku_fda0a19831eaf8c";
 $conn = new mysqli($server, $username, $password, $db);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-if ($conn->query("select * from new_table") === TRUE) {
-    echo "GOOOD MATHERFUCKA";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-
-$conn->close();
-echo "1";
-/*$conn->query('SELECT * FROM new_table');
-if ($result = $conn->query("SHOW TABLES"))
-{
+if ($result = $conn->query("SELECT * FROM cars")){
     while($row = $result->fetch_array(MYSQLI_NUM))
     {
         echo $row[0];
+		echo $row[1];
     }
 }
-
-$conn->close();*/
-echo "2";
+$conn->close();
+function mymodule_form_alter(&$form, $form_state) {
+  drupal_add_js(drupal_get_path('module', 'mymodule').'/jquery.maskedinput.min.js');
+  drupal_add_js(drupal_get_path('module', 'mymodule') . '/userreg.js');
+}
 ?>
