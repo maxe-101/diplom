6ae5dad870b654eb55e6ff4c4a384cf8f4fc3e43
maxe-101 <maxe-101@mail.ru>
2017-06-13 03:50:29 +0300

    razrab

--- userreg.js
+++ userreg.js
@@ -1,9 +1,9 @@
 (function($) {
 Drupal.behaviors.myBehavior = {
   attach: function (context) {
-  $(document).ready(function() {
+
     $("#edit-name").mask("99/99/9999");
-    });
+
   }
 };
 })(jQuery);
